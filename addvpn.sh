3c81b3b83f8a555f962bf13c7129a783b1b9ef3d
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-06-25 11:49:57 -0500

    Update addvpn.sh

--- addvpn.sh
+++ addvpn.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Put your VPN files directory here, without closing slash /. I recommend one like the one below.
+# the client-configs directory should be within this directory!
+vpndir=/home/administrator/VPN
+# EasyRSA Version - It will be the same as the directory, no /
+easyrsa=EasyRSA-3.0.8
 
 if readlink /proc/$$/exe | grep -qs "dash"; then
 	echo "This script needs to be run with bash, not sh! Bash it good!"
@@ -11,42 +16,47 @@ if [[ "$EUID" -ne 0 ]]; then
 fi
 if [ "$1" = "list" ]; then
         echo "A list of users:"
-        ls /home/administrator/VPN/client-configs/files/
+        ls $vpndir/client-configs/files/
         exit
 fi
 if [ "$1" = "" ]; then
         echo "to get a list of current users: sudo ./addvpn.sh list"
-	echo "Add user as one word, no spaces!"
-	echo "Eg: sudo ./addvpn.sh BobTheBuilder or sudo ./addvpn.sh Bob_Builder bob@email.com"
-	exit
-if [ "$2" = "" ]; then
-	echo "Their email address? If none, just type your own, it has to be valid!"
         echo "Add user as one word, no spaces!"
         echo "Eg: sudo ./addvpn.sh Bob_Builder bob@email.com"
-	exit
+        echo "No emailing? sudo ./addvpn.sh BobTheBuilder or sudo ./addvpn.sh Bob_Builder none"
+        exit
+if [ "$2" = "" ]; then
+        echo "The client email address? If no emailing: sudo ./addvpn.sh Bob_Builder none"
+        echo "Add user as one word, no spaces!"
+        echo "Eg: sudo ./addvpn.sh Bob_Builder bob@email.com"
+        exit
 fi
 
 else
-	cd /home/administrator/VPN/EasyRSA-3.0.8/ #Update this some fine day
+	cd $vpndir/$easyrsa/
 	./easyrsa build-client-full $1 nopass
 	# Generates the custom client.ovpn
-	cp /home/administrator/VPN/client-configs/client-common.txt /home/administrator/VPN/client-configs/files/$1.ovpn
-	echo "<ca>" >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	cat /home/administrator/VPN/EasyRSA-3.0.8/pki/ca.crt >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	echo "</ca>" >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	echo "<cert>" >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	cat /home/administrator/VPN/EasyRSA-3.0.8/pki/issued/$1.crt >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	echo "</cert>" >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	echo "<key>" >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	cat /home/administrator/VPN/EasyRSA-3.0.8/pki/private/$1.key >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	echo "</key>" >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	echo "<tls-auth>" >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	cat /home/administrator/VPN/EasyRSA-3.0.8/ta.key >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	echo "</tls-auth>" >> /home/administrator/VPN/client-configs/files/$1.ovpn
-	cd /home/administrator/VPN/
-	# you could use this, but why? chown nobody:nogroup /home/administrator/VPN/client-configs/files/*.ovpn
-	cd /home/administrator
+	cp $vpndir/client-configs/client-common.txt $vpndir/client-configs/files/$1.ovpn
+	echo "<ca>" >> $vpndir/client-configs/files/$1.ovpn
+	cat $vpndir/$easyrsa/pki/ca.crt >> $vpndir/client-configs/files/$1.ovpn
+	echo "</ca>" >> $vpndir/client-configs/files/$1.ovpn
+	echo "<cert>" >> $vpndir/client-configs/files/$1.ovpn
+	cat $vpndir/$easyrsa/pki/issued/$1.crt >> $vpndir/client-configs/files/$1.ovpn
+	echo "</cert>" >> $vpndir/client-configs/files/$1.ovpn
+	echo "<key>" >> $vpndir/client-configs/files/$1.ovpn
+	cat $vpndir/$easyrsa/pki/private/$1.key >> $vpndir/client-configs/files/$1.ovpn
+	echo "</key>" >> $vpndir/client-configs/files/$1.ovpn
+	echo "<tls-auth>" >> $vpndir/client-configs/files/$1.ovpn
+	cat $vpndir/$easyrsa/ta.key >> $vpndir/client-configs/files/$1.ovpn
+	echo "</tls-auth>" >> $vpndir/client-configs/files/$1.ovpn
+	cd $vpndir/
+	# you could use this, but why? chown nobody:nogroup $vpndir/client-configs/files/*.ovpn
+	cd $vpndir/
 	echo ""
-	echo "Client $1 added, emailing client files, configuration is available at" /home/administrator/VPN/client-configs/files/"$1.ovpn"
-        mutt -s "VPN for $1" -a "/home/administrator/VPN/client-configs/files/$1.ovpn" -c $2 -b administrator < /home/administrator/VPN/vpn-message.txt
+	if [ "$2" = "none" ]; then
+        	echo "Client $1 added, configuration is available here: $vpndir/client-configs/files/"$1.ovpn"
+	else
+        	echo "Client $1 added, emailing client files, configuration is available at" $vpndir/client-configs/files/"$1.ovpn"
+        	mutt -s "VPN for $1" -a "$vpndir/client-configs/files/$1.ovpn" -c $2 -b administrator < $vpndir/vpn-message.txt
+    fi
 fi
